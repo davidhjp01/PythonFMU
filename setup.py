2e092586ae3452806913d655d7b43db19dc039e9
David Heejong Park <hee.jong.park@dnv.com>
2024-10-31 18:05:40 +0100

    Github action update

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
-
+from setuptools.dist import Distribution
 # See setup.cfg for Python package parameters
-setup()
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(x):
+        return True
+
+setup(distclass=BinaryDistribution)
